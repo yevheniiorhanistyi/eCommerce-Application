388f68fadffca08c64962661a2a16328bf32715e
Yevhenii Orhanistyi <organistiy@gmail.com>
2023-08-09 12:41:06 +0200

    feat: add font and create global theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
+import { ThemeProvider, CssBaseline } from '@mui/material';
+import SignIn from './pages/SignIn/SignIn';
+import theme from './theme';
 import './styles/App.css';
 
 function App() {
-  return <div data-testid="app" className="App" />;
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <div data-testid="app" className="App">
+        <SignIn />
+      </div>
+    </ThemeProvider>
+  );
 }
 
 export default App;
