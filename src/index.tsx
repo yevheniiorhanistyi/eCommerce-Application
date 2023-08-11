819ea494b2ebc56f9f03eca0005de426ff698d4e
AlxndrSmk <voprosy262@gmail.com>
2023-08-11 09:05:53 +0300

    refactor: return Ract and ReactDOM imports how it was

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom/client';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import './index.scss';
 import router from './routes/router';
