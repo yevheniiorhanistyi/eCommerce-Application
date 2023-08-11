8e8dc7d6e399dd922e8a03a059c14b27e75ff958
Kostaintyn Kikinov <kikinov.k@gmail.com>
2023-08-11 16:05:24 +0300

    feat: implement style to theme

--- src/theme.ts
+++ src/theme.ts
@@ -14,6 +14,34 @@ const theme = createTheme({
       '"Segoe UI Emoji"',
       '"Segoe UI Symbol"',
     ].join(','),
+    h3: {
+      fontSize: 32,
+      fontWeight: 700,
+      display: 'block',
+      margin: '0 auto',
+      color: '#242424',
+      lineHeight: 1.167,
+      textTransform: 'uppercase',
+      textAlign: 'center',
+      marginBottom: '0.35em',
+    },
+  },
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          fontWeight: 700,
+          textTransform: 'uppercase',
+          lineHeight: 1.75,
+          color: '#fff',
+          width: '100%',
+          minWidth: 64,
+          padding: '16px 40px',
+          margin: '24px 0 16px',
+          borderRadius: '8px',
+        },
+      },
+    },
   },
 });
 
