53ea2bc14dcf02bb28dd2ffb72ccf79443a1775b
Kostaintyn Kikinov <kikinov.k@gmail.com>
2023-08-11 16:07:21 +0300

    feat: add registration page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import { ThemeProvider, CssBaseline, Box } from '@mui/material';
 import SignIn from './pages/SignIn/SignIn';
 import theme from './theme';
-import './styles/App.css';
+import RegistrationPage from './pages/RegistrationPage/RegistrationPage';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <Box data-testid="app" className="App">
+        <RegistrationPage />
         <SignIn />
       </Box>
     </ThemeProvider>
---
+++ src/pages/RegistrationPage/RegistrationPage.tsx
@@ -0,0 +1,23 @@
+import { Typography, Paper, Container, Link } from '@mui/material';
+
+import styles from './RegistrationPage.style';
+import Registration from '../../components/Registration/Registration';
+
+const RegistrationPage = () => (
+  <Container component="main" maxWidth="lg">
+    <Paper elevation={0} sx={{ p: 3 }}>
+      <Typography variant="h3" align="center" gutterBottom>
+        Registration
+      </Typography>
+      <Typography variant="body2" align="center" sx={styles.subtitle}>
+        {'already registered? '}
+        <Link href="/" align="center" underline="hover">
+          Sign In
+        </Link>
+      </Typography>
+      <Registration />
+    </Paper>
+  </Container>
+);
+
+export default RegistrationPage;
