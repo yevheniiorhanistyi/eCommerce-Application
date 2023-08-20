675f5f5871ee27163411ea2ba76321ac041a2934
Kostaintyn Kikinov <kikinov.k@gmail.com>
2023-08-20 17:47:49 +0300

    fix: incoming argument result in date type

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -1,5 +1,6 @@
 // eslint-disable-next-line import/prefer-default-export
-export const formatDateToYYYYMMDD = (date: Date): string => {
+export const formatDateToYYYYMMDD = (dateParam: Date): string => {
+  const date = new Date(dateParam);
   const year = date.getFullYear();
   const month = String(date.getMonth() + 1).padStart(2, '0');
   const day = String(date.getDate()).padStart(2, '0');
