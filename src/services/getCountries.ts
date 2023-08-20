4f6953d980ab60635436f785885018733aa7a746
Kostaintyn Kikinov <kikinov.k@gmail.com>
2023-08-20 18:21:31 +0300

    feat: change country selection

--- src/services/getCountries.ts
+++ src/services/getCountries.ts
@@ -1,14 +1,20 @@
 import { ModalContextType } from '../components/ModalProvider/type';
+import { ICountrie } from '../types/types';
 import iso3166Alpha2 from '../utils/countries';
 import { getProject } from './project';
 
-const getCountries = async (modal: ModalContextType): Promise<string[]> => {
+const getCountries = async (
+  modal: ModalContextType,
+): Promise<ICountrie[] | []> => {
   const body = await getProject(modal);
   if (body) {
     const codesCountrie = body.countries;
-    return codesCountrie.map(
-      (code) => `${code in iso3166Alpha2 ? iso3166Alpha2[code] : ''} (${code})`,
-    );
+    return codesCountrie.map((code) => ({
+      codeCountrie: code,
+      nameCountrie: `${
+        code in iso3166Alpha2 ? iso3166Alpha2[code] : ''
+      } (${code})`,
+    }));
   }
   return [];
 };
