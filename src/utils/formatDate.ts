58d88c818d94f62a93e6648d0e9eef6828d73c58
Kostaintyn Kikinov <kikinov.k@gmail.com>
2023-08-20 16:30:16 +0300

    feat: add formatDateToYYYYMMDD to utils

---
+++ src/utils/formatDate.ts
@@ -0,0 +1,8 @@
+// eslint-disable-next-line import/prefer-default-export
+export const formatDateToYYYYMMDD = (date: Date): string => {
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, '0');
+
+  return `${year}-${month}-${day}`;
+};
