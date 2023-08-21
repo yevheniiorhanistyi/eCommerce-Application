555678b318edf3811a2784aab7f61c2cf5f0f674
Alexander <voprosy262@gmail.com>
2023-08-22 00:18:30 +0300

    feat: add sign out logic

---
+++ src/components/buttons/SignOutButton/SignOutButton.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Button from '@mui/material/Button';
+import LogoutIcon from '@mui/icons-material/Logout';
+import { Link } from 'react-router-dom';
+import styles from './SignOutButton.styles';
+
+interface SignOutButtonProps {
+  onSignOutSuccess: () => void;
+}
+
+const SignInButton: React.FC<SignOutButtonProps> = ({
+  onSignOutSuccess,
+}: SignOutButtonProps) => (
+  <Link to="/" style={{ textDecoration: 'none' }}>
+    <Button
+      startIcon={<LogoutIcon />}
+      variant="text"
+      sx={styles.button}
+      onClick={onSignOutSuccess}
+    >
+      Sign out
+    </Button>
+  </Link>
+);
+
+export default SignInButton;
---
+++ src/components/buttons/SignUpButton/SignUpButton.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Button from '@mui/material/Button';
+import VpnKeyIcon from '@mui/icons-material/VpnKey';
+
+import { Link } from 'react-router-dom';
+import styles from './SignUpButton.styles';
+
+const SignUpButton: React.FC = () => (
+  <Link to="/signup" style={{ textDecoration: 'none' }}>
+    <Button
+      startIcon={<VpnKeyIcon />}
+      variant="outlined"
+      sx={styles.button}
+    >
+      Sign up
+    </Button>
+  </Link>
+);
+
+export default SignUpButton;
