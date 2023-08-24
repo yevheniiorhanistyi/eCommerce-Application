555b91df218994c265e0d649a1a5c1cdfd4640c7
Kostaintyn Kikinov <kikinov.k@gmail.com>
2023-08-24 19:44:04 +0300

    feat:  parse the fetched product data

--- src/components/DetailedProduct/DetailedProduct.tsx
+++ src/components/DetailedProduct/DetailedProduct.tsx
@@ -1,36 +1,84 @@
 import { useEffect, useState } from 'react';
-import { Box, Typography } from '@mui/material';
+import { Backdrop, Box, Typography } from '@mui/material';
 import { Product } from '@commercetools/platform-sdk';
 import { getProduct } from '../../services/product';
 import { useModal } from '../ModalProvider/ModalProvider';
 import DetailedProductNotFound from './DetailedProductNotFound';
+import languageCode from '../../utils/languageCode';
 
 interface DetailedProductProps {
   keyProduct: string | undefined;
+}
+
+interface IImage {
+  url: string;
+  dimensions: {
+    w: number;
+    h: number;
+  };
+}
+
+interface IProductDisplayData {
+  title: string;
+  description: string;
+  images: IImage[];
 }
 
 const DetailedProduct: React.FC<DetailedProductProps> = ({
   keyProduct,
 }: DetailedProductProps) => {
   const modal = useModal();
-  const [productData, setProductData] = useState<Product | null>(null);
+  const [productData, setProductData] = useState<
+  IProductDisplayData | null | ''
+  >('');
+
+  const parsingData = (data: Product | null): IProductDisplayData | null => {
+    if (data) {
+      return {
+        title: data.masterData.current.name[languageCode],
+        description: data.masterData.current.description
+          ? data.masterData.current.description[languageCode]
+          : '',
+        images: data.masterData.staged.masterVariant.images ?? [],
+      };
+    }
+    return null;
+  };
 
   useEffect(() => {
     const fetchData = async () => {
       if (keyProduct !== undefined) {
         const data = await getProduct(keyProduct, modal);
-        setProductData(data);
+        const dataDisplay = parsingData(data);
+        setProductData(dataDisplay);
       }
     };
 
     fetchData();
   }, [keyProduct, modal]);
-  return productData ? (
+
+  if (productData === null) {
+    return <DetailedProductNotFound />;
+  }
+  if (keyProduct === undefined) {
+    return <DetailedProductNotFound />;
+  }
+  return (
     <Box>
-      <Typography variant="h5">{keyProduct}</Typography>
+      <Typography variant="h5">
+        {productData ? productData.title : ''}
+      </Typography>
+      <Typography variant="body2">
+        {productData ? productData.description : ''}
+      </Typography>
+      {productData
+        ? productData.images.map((item) => (
+          <Typography key={item.url} variant="body2">
+            {item.url}
+          </Typography>
+        ))
+        : ''}
     </Box>
-  ) : (
-    <DetailedProductNotFound />
   );
 };
 
