8ddb7e12fe534fa0819cdc43589373c9c5bbbb76
Yevhenii Orhanistyi <organistiy@gmail.com>
2023-08-28 17:47:57 +0200

    feat: add service to get products

---
+++ src/services/products/getProducts.ts
@@ -0,0 +1,28 @@
+import axios from 'axios';
+import { getTokenFromLocalStorage } from '../../utils/authUtils';
+import { IProductResponse } from '../../types/productInterfaces';
+import { getAnonymousToken } from '../authenticate/getAnonymousToken';
+
+const projectKey = import.meta.env.VITE_REACT_APP_PROJECT_KEY;
+const baseUrl = import.meta.env.VITE_REACT_APP_API_URL;
+
+export const getProducts = async () => {
+  try {
+    const { token } = getTokenFromLocalStorage() || (await getAnonymousToken());
+    const response = await axios.get<IProductResponse>(
+      `${baseUrl}/${projectKey}/products`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+      },
+    );
+    return response.data.results;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
+
+export default getProducts;
---
+++ src/types/productInterfaces.ts
@@ -0,0 +1,65 @@
+interface ICategory {
+  id: string;
+  typeId: string;
+}
+
+interface IAttribute {
+  name: string;
+  value: {
+    key: string;
+    label: string;
+  };
+}
+
+interface IImage {
+  dimensions: {
+    h: number;
+    w: number;
+  };
+  url: string;
+}
+
+export interface IPrice {
+  id: string;
+  discounted: {
+    discount: { id: string; typeId: string };
+    value: {
+      centAmount: number;
+      currencyCode: string;
+      fractionDigits: number;
+      type: string;
+    };
+  };
+  value: {
+    centAmount: number;
+    currencyCode: string;
+    fractionDigits: number;
+    type: string;
+  };
+}
+
+export interface IProduct {
+  id: string;
+  key: string;
+  createdAt: string;
+  masterData: {
+    current: {
+      categories: ICategory[];
+      name: { 'en-US': string };
+      description: { 'en-US': string };
+      masterVariant: {
+        attributes: IAttribute[];
+        images: IImage[];
+        prices: IPrice[];
+      };
+    };
+  };
+}
+
+export interface IProductResponse {
+  count: number;
+  limit: number;
+  offset: number;
+  total: number;
+  results: IProduct[];
+}
