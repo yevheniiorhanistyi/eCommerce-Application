040c11b6c67b7afc95eea26e5c838e4bb299bd87
Kostaintyn Kikinov <kikinov.k@gmail.com>
2023-08-29 13:34:34 +0300

    feat: add slider component

--- src/components/ProductSlider/ProductSlider.tsx
+++ src/components/ProductSlider/ProductSlider.tsx
@@ -1,14 +1,35 @@
+import Carousel from 'react-material-ui-carousel';
 import { IImage } from '../DetailedProduct/services/parsingData';
 import ProductImage from '../ProductImage/ProductImage';
+
+import './_product_slider.scss';
 
 interface ProductSliderProps {
   images: IImage[];
   keyProduct: string;
 }
-// TODO  Implement an image slider for product images
+
 const ProductSlider: React.FC<ProductSliderProps> = ({
   images,
   keyProduct,
-}: ProductSliderProps) => <ProductImage url={images[0].url} alt={keyProduct} />;
+}: ProductSliderProps) => (
+  <Carousel
+    autoPlay={false}
+    animation="slide"
+    navButtonsAlwaysVisible
+    cycleNavigation={false}
+    navButtonsWrapperProps={{ className: 'nav__wrap' }}
+    navButtonsProps={{ className: 'nav__button' }}
+    indicatorContainerProps={{ className: 'indicator__container' }}
+    indicatorIconButtonProps={{ className: 'indicator__button' }}
+    activeIndicatorIconButtonProps={{ className: 'indicator__button-active' }}
+    height="100%"
+    sx={{ height: '100%' }}
+  >
+    {images.map((item) => (
+      <ProductImage key={item.url} url={item.url} alt={keyProduct} />
+    ))}
+  </Carousel>
+);
 
 export default ProductSlider;
