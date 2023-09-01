57e9ef8fdcbe83799a97a858fc6c7727196721c4
Kostaintyn Kikinov <kikinov.k@gmail.com>
2023-09-01 13:38:43 +0300

    feat: add custom color `accent` to theme

--- src/theme.ts
+++ src/theme.ts
@@ -1,6 +1,11 @@
 import { createTheme } from '@mui/material/styles';
 
 const theme = createTheme({
+  palette: {
+    accent: {
+      main: '#d32f2f',
+    },
+  },
   typography: {
     fontFamily: [
       'Urbanist',
