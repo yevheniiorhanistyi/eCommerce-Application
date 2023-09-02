ced04c0fd849d8e3dcea1c22e33e30a82b378412
Yevhenii Orhanistyi <organistiy@gmail.com>
2023-09-02 09:06:54 +0200

    feat: move constants in separate file

---
+++ src/constants/constants.ts
@@ -0,0 +1,27 @@
+export const sortingOptions = [
+  { label: 'Brand: A - Z', value: 'brand-asc' },
+  { label: 'Brand: Z - A', value: 'brand-desc' },
+  { label: 'Price: Low - High', value: 'price-asc' },
+  { label: 'Price: High - Low', value: 'price-desc' },
+];
+
+export const clothingCategories = ["Men's", "Women's", "Kids'"];
+export const clothingSizes = ['Small', 'Medium', 'Large'];
+export const brands = [
+  'Dalbello',
+  'Outdoor',
+  'REI Co-op',
+  'Athleta',
+  'Patagonia',
+  'KUHL',
+];
+export const colors = [
+  'Red',
+  'Blue',
+  'Green',
+  'Black',
+  'White',
+  'Yellow',
+  'Loden',
+  'Multi-Color',
+];
