064407a5e57980724eed9305e5ee80d740002ac3
Kostaintyn Kikinov <kikinov.k@gmail.com>
2023-09-02 17:29:02 +0300

    feat: add button to product image

--- src/components/ProductImage/ProductImage.tsx
+++ src/components/ProductImage/ProductImage.tsx
@@ -1,14 +1,43 @@
-import { Box } from '@mui/material';
+import { useState } from 'react';
+import { Card, CardActionArea, CardMedia } from '@mui/material';
+import { ZoomOutMap } from '@mui/icons-material';
 import { IProductImageProps } from '../../types/types';
 import styles from './ProductImage.styles';
 
 const ProductImage: React.FC<IProductImageProps> = ({
   url,
   alt,
-}: IProductImageProps) => (
-  <Box key={url} sx={styles.imageWrap}>
-    <img src={url} alt={alt} style={styles.image} />
-  </Box>
-);
+}: IProductImageProps) => {
+  const [hovered, setHovered] = useState(false);
+
+  const handleHover = () => {
+    setHovered(true);
+  };
+
+  const handleUnhover = () => {
+    setHovered(false);
+  };
+
+  const handleClick = () => {};
+
+  return (
+    <Card sx={styles.imageWrap}>
+      <CardActionArea
+        sx={styles.imageWrap}
+        onMouseEnter={handleHover}
+        onMouseLeave={handleUnhover}
+        onClick={handleClick}
+      >
+        <CardMedia sx={styles.image} image={url} title={alt} />
+        <ZoomOutMap
+          sx={{
+            ...styles.hover,
+            opacity: hovered ? 0.5 : 0,
+          }}
+        />
+      </CardActionArea>
+    </Card>
+  );
+};
 
 export default ProductImage;
