b23c47eeeed48e15f263d93e6ae5cc3a33356cf2
Yevhenii Orhanistyi <organistiy@gmail.com>
2023-09-02 09:04:48 +0200

    refactor: raise the state and service call in the parent component

--- src/components/ProductList/ProductList.styles.ts
+++ src/components/ProductList/ProductList.styles.ts
@@ -1,20 +1,17 @@
 const styles = {
   circularIndeterminateBox: {
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-    margin: 'auto',
+    position: 'relative',
     display: 'flex',
     justifyContent: 'center',
-    alignItems: 'center',
-    height: '100%',
   },
   productListBox: {
     display: 'flex',
-    justifyContent: 'center',
     flexWrap: 'wrap',
-    padding: '20px 0',
+    justifyContent: 'space-around',
+
+    '@media (max-width: 1175px)': {
+      justifyContent: 'space-around',
+    },
   },
 };
 
