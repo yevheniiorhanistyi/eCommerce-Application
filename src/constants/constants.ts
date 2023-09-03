acf2fdbc63757a766babd182e4c639d530695370
Yevhenii Orhanistyi <organistiy@gmail.com>
2023-09-03 17:52:40 +0200

    feat: add new values according to the new data

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,27 +1,37 @@
 export const sortingOptions = [
-  { label: 'Brand: A - Z', value: 'brand-asc' },
-  { label: 'Brand: Z - A', value: 'brand-desc' },
-  { label: 'Price: Low - High', value: 'price-asc' },
-  { label: 'Price: High - Low', value: 'price-desc' },
+  { label: 'Name: A - Z', value: 'name.en-us asc' },
+  { label: 'Name: Z - A', value: 'name.en-us desc' },
+  { label: 'Price: Low - High', value: 'price asc' },
+  { label: 'Price: High - Low', value: 'price desc' },
 ];
 
-export const clothingCategories = ["Men's", "Women's", "Kids'"];
-export const clothingSizes = ['Small', 'Medium', 'Large'];
+export const clothingCategories = [
+  { label: "Men's", id: '10f454b6-db13-46d0-a905-828224ab7231' },
+  { label: "Women's", id: '90a47f27-61f2-4ad6-8bd7-75fc9bca4f19' },
+  { label: 'Boys', id: 'e8ee2706-5eb4-4bd0-9a13-44332541f6bd' },
+];
+export const clothingSizes = [
+  { label: 'XS', value: 'XS' },
+  { label: 'S', value: 'S' },
+  { label: 'M', value: 'M' },
+  { label: 'L', value: 'L' },
+  { label: 'XL', value: 'XL' },
+  { label: 'XXL', value: 'XXL' },
+];
 export const brands = [
-  'Dalbello',
-  'Outdoor',
-  'REI Co-op',
-  'Athleta',
-  'Patagonia',
-  'KUHL',
+  { label: 'Athleta', value: 'Athleta' },
+  { label: 'Dalbello', value: 'Dalbello' },
+  { label: 'Outdoor', value: 'Outdoor' },
+  { label: 'REI Co-op', value: 'REI Co-op' },
+  { label: 'Patagonia', value: 'Patagonia' },
+  { label: 'KUHL', value: 'KUHL' },
 ];
 export const colors = [
-  'Red',
-  'Blue',
-  'Green',
-  'Black',
-  'White',
-  'Yellow',
-  'Loden',
-  'Multi-Color',
+  { label: 'Black', value: 'Black' },
+  { label: 'Red', value: 'Red' },
+  { label: 'Blue', value: 'Blue' },
+  { label: 'Green', value: 'Green' },
+  { label: 'Pink', value: 'Pink' },
+  { label: 'Orange', value: 'Orange' },
+  { label: 'Gray', value: 'Gray' },
 ];
