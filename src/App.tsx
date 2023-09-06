79813cee068a36422a0ab2c02417b5a72a25b7b5
Alexander <voprosy262@gmail.com>
2023-09-06 22:15:41 +0300

    refactor: move routes to a separate file

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,12 @@
 import { ThemeProvider, CssBaseline, Box } from '@mui/material';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { RouterProvider } from 'react-router-dom';
 import { SnackbarProvider } from 'notistack';
 import { AuthProvider } from './components/AuthProvider/AuthProvider';
 import { ModalProvider } from './components/ModalProvider/ModalProvider';
 import theme from './theme';
 
-import Main from './pages/Main/Main';
-import Catalog from './pages/Catalog/Catalog';
-import DetailedProductPage from './pages/DetailedProductPage/DetailedProductPage';
-import NotFound404 from './pages/NotFound404/NotFound404';
-import Profile from './pages/Profile/Profile';
-import SignIn from './pages/SignIn/SignIn';
-import SignUp from './pages/SignUp/SignUp';
-import PrimaryLayout from './layouts/PrimaryLayout';
 import { CategoryDataProvider } from './components/CategoryDataProvider/CategoryDataProvider';
+import router from './routes/router';
 
 const App: React.FC = () => (
   <SnackbarProvider maxSnack={3}>
@@ -23,23 +16,7 @@ const App: React.FC = () => (
         <ModalProvider>
           <CategoryDataProvider>
             <Box data-testid="app" className="App">
-              <BrowserRouter>
-                <Routes>
-                  <Route element={<PrimaryLayout />}>
-                    <Route path="/" element={<Main />} />
-                    <Route path="/signin" element={<SignIn />} />
-                    <Route path="/signup" element={<SignUp />} />
-                    <Route
-                      path="/product/:key"
-                      element={<DetailedProductPage />}
-                    />
-                    <Route path="/catalog" element={<Catalog />} />
-                    <Route path="/category/:key" element={<Catalog />} />
-                    <Route path="/profile" element={<Profile />} />
-                    <Route path="/*" element={<NotFound404 />} />
-                  </Route>
-                </Routes>
-              </BrowserRouter>
+              <RouterProvider router={router} />
             </Box>
           </CategoryDataProvider>
         </ModalProvider>
