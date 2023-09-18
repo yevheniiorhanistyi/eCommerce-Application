11a564030d5c0181e77227ff29158ea06fd21c3b
Kostaintyn Kikinov <kikinov.k@gmail.com>
2023-09-18 18:55:29 +0300

    feat: add request for cleaning confirmation

--- src/components/ClearCart/ClearCart.tsx
+++ src/components/ClearCart/ClearCart.tsx
@@ -2,6 +2,8 @@ import { useState } from 'react';
 import { Button, Typography } from '@mui/material';
 import RemoveShoppingCartIcon from '@mui/icons-material/RemoveShoppingCart';
 import styles from './ClearCart.styles';
+import { useModal } from '../ModalProvider/ModalProvider';
+import { TReturnClose } from '../ModalProvider/type';
 
 interface IClearCartProps {
   clearSuccess: () => void;
@@ -11,8 +13,29 @@ const ClearCart: React.FC<IClearCartProps> = ({
   clearSuccess,
 }: IClearCartProps) => {
   const [isDisabled, setIsDisabled] = useState(false);
+  const modal = useModal();
 
-  const handleClick = () => {};
+  const handleClick = () => {
+    setIsDisabled(true);
+    modal.openModal('confirm', false);
+    modal.setContent(
+      'confirm',
+      {
+        title: 'Confirmation request',
+        text: 'Are you sure you want to clear your cart?',
+      },
+      (isSuccess: TReturnClose) => {
+        if (isSuccess) {
+          clearCart();
+        }
+      },
+    );
+    setIsDisabled(false);
+  };
+
+  const clearCart = async () => {
+    console.log('clearCart');
+  };
 
   return (
     <Button
---
+++ src/components/ConfirmModal/ConfirmModal.styles.ts
@@ -0,0 +1,27 @@
+const styles = {
+  box: {
+    position: 'absolute',
+    left: '50%',
+    top: '50%',
+    width: 500,
+    backgroundColor: '#fff',
+    p: 3,
+    borderRadius: 2,
+    transform: 'translate(-50%, -50%)',
+    '@media (max-width:500px)': {
+      width: '100%',
+    },
+  },
+  button: {
+    display: 'block',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    padding: '10px 40px',
+  },
+  controlWrap: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+};
+
+export default styles;
