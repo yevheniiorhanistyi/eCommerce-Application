1b6d5286c3bc00b5efe11ffc370a531ad4da6458
Yevhenii Orhanistyi <organistiy@gmail.com>
2023-09-27 12:35:55 +0200

    feat: add BestSellers component in Main page

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,9 +1,11 @@
 import { Container } from '@mui/material';
-import PromoSlider from '../../components/PromoSlider/PromoSlider';
+
+import { PromoSlider, BestSellers } from '../../components';
 
 const Main: React.FC = () => (
   <Container maxWidth="lg">
     <PromoSlider />
+    <BestSellers />
   </Container>
 );
 export default Main;
