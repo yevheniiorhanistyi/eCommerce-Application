0df2646ae2d3518be88844b687197629cb5c540f
Yevhenii Orhanistyi <organistiy@gmail.com>
2023-09-27 12:32:38 +0200

    feat: add BestSellers component

---
+++ src/components/BestSellers/BestSellers.tsx
@@ -0,0 +1,11 @@
+import { Box, Typography } from '@mui/material';
+
+export const BestSellers: React.FC = () => (
+  <Box sx={{ padding: '20px 10px' }}>
+    <Typography variant="h3" align="center">
+      Featured Products
+    </Typography>
+  </Box>
+);
+
+export default BestSellers;
