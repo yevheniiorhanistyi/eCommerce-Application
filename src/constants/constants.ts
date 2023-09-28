8fc20a7ae66932ac3d3758978769fd6329f1d258
Yevhenii Orhanistyi <organistiy@gmail.com>
2023-09-28 09:58:48 +0200

    refactor: remove unused constant

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -9,12 +9,6 @@ export const sortingOptions = [
   { label: 'Price: Low - High', value: 'price asc' },
   { label: 'Price: High - Low', value: 'price desc' },
 ];
-
-export const clothingCategories = [
-  { label: "Men's", id: '10f454b6-db13-46d0-a905-828224ab7231' },
-  { label: "Women's", id: '90a47f27-61f2-4ad6-8bd7-75fc9bca4f19' },
-  { label: 'Boys', id: 'e8ee2706-5eb4-4bd0-9a13-44332541f6bd' },
-];
 export const clothingSizes = [
   { label: 'XS', value: 'XS' },
   { label: 'S', value: 'S' },
