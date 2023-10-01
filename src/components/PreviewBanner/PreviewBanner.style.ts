8978788b3cf81b46d554da87f35927f2b69c73be
Yevhenii Orhanistyi <organistiy@gmail.com>
2023-10-01 08:36:35 +0200

    feat: add PreviewBanner component

---
+++ src/components/PreviewBanner/PreviewBanner.style.ts
@@ -0,0 +1,38 @@
+const styles = {
+  outerBox: {
+    width: '100%',
+    height: '80vh',
+    position: 'relative',
+  },
+  img: {
+    display: 'block',
+    width: '100%',
+    height: '100%',
+    objectFit: 'cover',
+    objectPosition: 'center',
+  },
+  innerBox: {
+    display: 'flex',
+    flexDirection: 'column',
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+  },
+  title: {
+    fontSize: '1.5rem',
+    fontWeight: '600',
+    letterSpacing: '.1em',
+    color: '#fff',
+  },
+  button: {
+    fontSize: '0.8rem',
+    maxWidth: '140px',
+    padding: '10px 30px',
+    margin: '15px auto',
+    border: '2px solid #fff',
+    borderRadius: '0',
+  },
+};
+
+export default styles;
