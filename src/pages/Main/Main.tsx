975a0bbfb16e195bf6b6ce61b51ad7a38a26f197
Yevhenii Orhanistyi <organistiy@gmail.com>
2023-10-01 11:49:43 +0200

    feat: add FeatureBanner component at the Main page

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,12 +1,16 @@
-import { Container, Box } from '@mui/material';
-
-import { PromoSlider, BestSellers, PreviewBanner } from '../../components';
+import {
+  BestSellers,
+  PreviewBanner,
+  OurBrands,
+  FeatureBanner,
+} from '../../components';
 
 const Main: React.FC = () => (
   <>
-    {/* <PromoSlider /> */}
     <PreviewBanner />
     <BestSellers />
+    <FeatureBanner />
+    <OurBrands />
   </>
 );
 export default Main;
